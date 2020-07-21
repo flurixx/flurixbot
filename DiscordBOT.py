6c5a392d576c434c811a5838494c41b24f04424d
flurixx <68532186+flurixx@users.noreply.github.com>
2020-07-21 22:40:53 +0300

    Update DiscordBOT.py

--- DiscordBOT.py
+++ DiscordBOT.py
@@ -20,7 +20,7 @@ bot.remove_command('help')
 
 @bot.command()
 @has_permissions(manage_roles=True)
-async def start(ctx,name:str('muted')):
+async def start(ctx,name:str):
 		for role in ctx.guild.roles:
 			if name.lower() in role.name.lower():
 				await ctx.send(embed = discord.Embed(description = 'Бот уже настроен!', color = 0x49FF33))
