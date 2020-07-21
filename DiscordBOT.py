00037a80e8a4d3a5ffc0b5a3da729d15a0c99b1e
flurixx <68532186+flurixx@users.noreply.github.com>
2020-07-21 17:44:12 +0300

    Update DiscordBOT.py

--- DiscordBOT.py
+++ DiscordBOT.py
@@ -33,25 +33,7 @@ async def game(ctx):
 		await channel.send(embed = discord.Embed(description = ':new_moon: Выпал орёл!', color = 0x49FF33))
 	else:
 		await channel.send(embed = discord.Embed(description = ':first_quarter_moon: Монета встала ребром! Шанс этого всего 1%!', color = 0x49FF33))
-		
-@bot.command()
-async def game2(ctx,message):
-	channel = bot.get_channel( 734072439620763733 )
-	await bot.process_commands( message )
-	msg = message.content.lower()
-	await channel.send(embed = discord.Embed(description = '''Игра flurixQuest началась!
-Вы - отважный путешественик, взяли квест на легкий, казалось бы фарм мобов в подземелье.
-Вот вы уже спустились в поздемелье, как вдруг за вашей спиной закрываются ворота
-Ваши действия:
-1) Попытаться открыть ворота
-2) Зажечь факел и идти дальше''', color = 0xFF8C00))
-	deystviye1 = ['1']
-	deystviye2 = ['2']
-	if msg in deystviye1:
-		await channel.send(embed = discord.Embed(description = 'Вы попытались открыть ворота и умерли от истощения :(', color = 0xFF0000))
-	if msg in deystviye2:
-		await channel.send(embed = discord.Embed(description = 'Вы зажгли факел и сгорели :(', color = 0xFF0000))
-		
+				
 @bot.command(aliases = ['помощь','помоги','команды'])
 async def info(ctx):
 	embed1 = discord.Embed(title = 'Обо мне', description = '''Привет!
