b712308016794b6e8509c2708f9b4a918ed38bf3
flurixx <68532186+flurixx@users.noreply.github.com>
2020-07-22 12:41:26 +0300

    Update DiscordBOT.py

--- DiscordBOT.py
+++ DiscordBOT.py
@@ -32,6 +32,7 @@ async def start(ctx,name="muted"):
 				await guild.create_role(name="muted", permissions=perms)
 				await ctx.send(embed = discord.Embed(description = '''Первоначальная настройка бота завершена!
 Удачного пользования :)''', color = 0x49FF33))
+				return None
 			else:			
 				await ctx.send(embed = discord.Embed(description = 'Бот уже настроен!', color = 0x49FF33))
 				return role
